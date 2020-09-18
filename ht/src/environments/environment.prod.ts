956f724aae469f1e4d65ff7ef900dbbb3c518454
nusrat3344 <45029200+nusrat3344@users.noreply.github.com>
2020-09-19 00:06:41 +0600

    This is the complete "Happy Tummies" Project.
    Co-Authored-By: syntax-error35 <46873800+syntax-error35@users.noreply.github.com>

---
+++ ht/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyADf64hsl_AuB9d0wOHmKzYzTBC67fGnos",
+    authDomain: "happy-tummies.firebaseapp.com",
+    databaseURL: "https://happy-tummies.firebaseio.com",
+    projectId: "happy-tummies",
+    storageBucket: "happy-tummies.appspot.com",
+    messagingSenderId: "544180863070",
+    appId: "1:544180863070:web:c3620283fa79ce773fb4cf",
+    measurementId: "G-7SF7YLF5FM"    
+  }
+};
---
+++ ht/src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyADf64hsl_AuB9d0wOHmKzYzTBC67fGnos",
+    authDomain: "happy-tummies.firebaseapp.com",
+    databaseURL: "https://happy-tummies.firebaseio.com",
+    projectId: "happy-tummies",
+    storageBucket: "happy-tummies.appspot.com",
+    messagingSenderId: "544180863070",
+    appId: "1:544180863070:web:c3620283fa79ce773fb4cf",
+    measurementId: "G-7SF7YLF5FM"   
+  }
+};
